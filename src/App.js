6d1dc385f75a7e71f50bd85c7a8ab75062eb6315
MLZ6 <kopomasuga64@gmail.com>
2024-07-17 22:41:33 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,29 +4,31 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/MLZ6" rel="noreferrer" target="_blank">
-          Maitumelo Moaletsi
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MLZ6/react-weather-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://app.netlify.com/sites/bright-tulumba-9f2a26/overview"
-          rel="noreferrer"
-          target="_blank"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/MLZ6" rel="noreferrer" target="_blank">
+            Maitumelo Moaletsi
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MLZ6/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://app.netlify.com/sites/bright-tulumba-9f2a26/overview"
+            rel="noreferrer"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
