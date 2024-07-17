2497264b973310d0a801eb9d33f5cde8cdb33844
MLZ6 <kopomasuga64@gmail.com>
2024-07-17 21:24:53 +0200

    Added the footer to App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,31 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/MLZ6" rel="noreferrer" target="_blank">
+          Maitumelo Moaletsi
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MLZ6/react-weather-app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://app.netlify.com/sites/bright-tulumba-9f2a26/overview"
+          rel="noreferrer"
+          target="_blank"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
