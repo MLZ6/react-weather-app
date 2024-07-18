98aadb3f77d13b2b62b9ae991253ebe41312fd56
MLZ6 <kopomasuga64@gmail.com>
2024-07-18 08:17:04 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,30 +5,46 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="'form-control"
-        />
-        <input type="button" value="search" className="btn btn-primary" />
+        <div className="row d-flex">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="'form-control w-100"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>New York</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="clearfix d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation:15%</li>
-            <li>Humidity:72%</li>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
             <li>Wind: 13km/h</li>
           </ul>
         </div>
